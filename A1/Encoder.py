9599daf82fbd8ea5166fe8070eb29a4150218107
BrianQu <qujianning0401@163.com>
2023-10-23 16:27:04 -0400

    update

--- A1/Encoder.py
+++ A1/Encoder.py
@@ -1,5 +1,4 @@
 import numpy as np
-import scipy
 
 from GlobalConfig import Config
 from YUV_formater import YUV_Operator
@@ -49,47 +48,7 @@ class Encoder:
     def _reconstruct_block_(self, predict_block:Block, residual_block:Block):
         return Block(residual_block.x, residual_block.y, residual_block.block_size, \
                      None, predict_block.block_data+residual_block.block_data)
-    
 
-    def _apply_dct_to_residual_block(self, residual_block):
-        # using scipy to do the 2D DCT
-        dct_block = scipy.fftpack.dctn(residual_block, type=2, norm='ortho')
-        # round the result
-        rounded_dct_block = np.round(dct_block).astype(int)
-        return rounded_dct_block
-    
-
-    def _apply_idct_to_dct_block(self, dct_block):
-        # using scipy to do the 2D IDCT
-        idct_block = scipy.fftpack.idctn(dct_block, type=2, norm='ortho')
-        return idct_block
-    
-    def _quantize_coefficients(self, dct_block, QP):
-        i = dct_block.shape[0]
-        Q = self._calculate_quantization_matrix(i, QP)
-        quantized_block = np.round(dct_block / Q)
-        return quantized_block
-    
-
-    def _dequantize_coefficients(self, quantized_block, QP):
-        i = quantized_block.shape[0]
-        Q = self._calculate_quantization_matrix(i, QP)
-        dequantized_block = quantized_block * Q
-        return dequantized_block
-    
-
-    def _calculate_quantization_matrix(self, i, QP):
-        Q = np.zeros((i, i), dtype=float)
-        for x in range(i):
-            for y in range(i):
-                if x + y < i - 1:
-                    Q[x, y] = 2 * QP
-                elif x + y == i - 1:
-                    Q[x, y] = 2 * (QP + 1)
-                else:
-                    Q[x, y] = 2 * (QP + 2)
-        return Q
-    
     
     def encode(self, input_video_path, ws = 'Assignment/encoder_ws'):
         # Read from video, default 420p
