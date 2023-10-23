67122e224d9f9f3ce1771791bda2b2a97be374f6
BrianQu <qujianning0401@163.com>
2023-10-23 16:59:24 -0400

    feature: 4-a

--- A1/Decoder.py
+++ A1/Decoder.py
@@ -3,6 +3,7 @@ import numpy as np
 from GlobalConfig import Config
 from Block import pad_frame, Block
 from YUV_formater import YUV_Operator
+from scipy.fftpack import idct
 config = Config()
 
 class Decoder:
@@ -17,8 +18,55 @@ class Decoder:
     def _reconstruct_block_(self, predict_block:Block, residual_block:Block):
         return Block(residual_block.x, residual_block.y, residual_block.block_size, \
                      None, predict_block.block_data+residual_block.block_data)
+    
+    def idct_transform(self, block):
+        return idct(idct(block, axis=0, norm='ortho'), axis=1, norm='ortho')
+    
+    def dequantize(self, quantized_coefficients, QP, i):
+        Q = self.calculate_quantization_matrix(QP, i)
+        return quantized_coefficients * Q
+    
+    def decode_i_frame(self, i_frame_data):
+        frame_height = self.config.height
+        frame_width = self.config.width
+        block_size = self.config.block_size
+        frames = []
 
-    def decode(self, input_dir = 'Assignment/encoder_ws' ,ws = 'Assignment/decoder_ws'):
+        # TODO: 从i_frame_data中接收QP值，现在设置个default值
+        QP = 0
+
+        for i in range(0, frame_height, block_size):
+            for j in range(0, frame_width, block_size):
+                quantized_coefficients = i_frame_data.pop(0)  # 从帧数据中取出一个块的量化系数
+
+                # 2. 对每个块执行反量化操作
+                # TODO: 使用接收到的QP值进行反量化操作
+
+                dct_coefficients = self.dequantize(quantized_coefficients, QP, block_size)
+
+                # 3. 对每个块执行IDCT逆变换
+                reconstructed_block = self.idct_transform(dct_coefficients)
+
+                # 4. 将块重构为帧
+                frames.append(reconstructed_block)
+
+        # 5. TODO: 保存QP值以备将来使用
+
+        return frames
+
+    def calculate_quantization_matrix(self, QP, i):
+        Q = np.zeros((i, i), dtype=float)
+        for x in range(i):
+            for y in range(i):
+                if (x + y) < (i - 1):
+                    Q[x][y] = 2 * QP
+                elif (x + y) == (i - 1):
+                    Q[x][y] = 2 * (QP + 1)
+                else:
+                    Q[x][y] = 2 * (QP + 2)
+        return Q
+
+    def decode(self, input_dir='Assignment/encoder_ws', ws='Assignment/decoder_ws'):
         block_size = self.config.block_size
         width = self.config.width
         height = self.config.height
@@ -29,19 +77,29 @@ class Decoder:
         with open(f'{input_dir}/mv.13', 'rb') as f_mv:
             with open(f'{input_dir}/residual_frames.13', 'rb') as f_residaul_frames:
                 while True:
+                    # TODO: 读取QP值，现在设置个default值
+                    QP = 0
+
                     residual_frame = f_residaul_frames.read(reference_frame.size)
                     if not residual_frame:
                         break
                     residual_data = np.frombuffer(residual_frame, dtype=np.uint8).reshape(reference_frame.shape)
                     reconstructed_frame = np.zeros(reference_frame.shape, dtype=np.uint8)
+                    i_frame_data = []  # 用于存储I帧的帧数据
                     for i in range(0, self.config.height, block_size):
                         for j in range(0, self.config.width, block_size):
                             motion_vector = np.frombuffer(f_mv.read(2), dtype=np.int8)
-                            reference_block = Block(i+motion_vector[0], j+motion_vector[1], block_size, reference_frame)
-                            residual_block = residual_data[i:i+block_size, j:j+block_size]
-                            reconstructed_block = Block(i, j, block_size, None, reference_block.block_data+residual_block)
-                            reconstructed_frame[i:i+block_size, j:j+block_size] = reconstructed_block.block_data
-                            # self.yuv_operator.visualize_YUV_in_text(reference_block.block_data)
+                            reference_block = Block(i + motion_vector[0], j + motion_vector[1], block_size, reference_frame)
+                            residual_block = residual_data[i:i + block_size, j:j + block_size]
+                            reconstructed_block = Block(i, j, block_size, None, reference_block.block_data + residual_block)
+                            reconstructed_frame[i:i + block_size, j:j + block_size] = reconstructed_block.block_data
+                            i_frame_data.append(reconstructed_block.block_data)
+
+                    # 调用decode_i_frame方法，传递帧数据和QP值
+                    i_frames = self.decode_i_frame(i_frame_data, QP)
+
+                    # TODO: 使用i_frames进行进一步的处理或保存
+                    
                     reference_frame = reconstructed_frame
                     frames.append(reconstructed_frame)
                     self.yuv_operator.convert_Y_to_png(reconstructed_frame, f'{ws}/png_sequence/Y_frame_reconstructed{k}.png')
--- A1/Encoder.py
+++ A1/Encoder.py
@@ -3,6 +3,7 @@ import numpy as np
 from GlobalConfig import Config
 from YUV_formater import YUV_Operator
 from Block import Block, pad_frame, PSNR, SSIM
+from scipy.fftpack import dct, idct
 config = Config()
 
 class Encoder:
@@ -48,9 +49,60 @@ class Encoder:
     def _reconstruct_block_(self, predict_block:Block, residual_block:Block):
         return Block(residual_block.x, residual_block.y, residual_block.block_size, \
                      None, predict_block.block_data+residual_block.block_data)
-
     
-    def encode(self, input_video_path, ws = 'Assignment/encoder_ws'):
+    
+    def dct_transform(self, block):
+        return dct(dct(block, axis=0, norm='ortho'), axis=1, norm='ortho')
+    
+
+    def idct_transform(self, block):
+        return idct(idct(block, axis=0, norm='ortho'), axis=1, norm='ortho')
+    
+
+    def quantize(self, coefficients, QP, i):
+        Q = self.calculate_quantization_matrix(QP, i)
+        return np.round(coefficients / Q)
+    
+
+    def calculate_quantization_matrix(self, QP, i):
+        Q = np.zeros((i, i), dtype=float)
+        for x in range(i):
+            for y in range(i):
+                if (x + y) < (i - 1):
+                    Q[x][y] = 2 * QP
+                elif (x + y) == (i - 1):
+                    Q[x][y] = 2 * (QP + 1)
+                else:
+                    Q[x][y] = 2 * (QP + 2)
+        return Q
+    
+
+    def encode_i_frame(self, frame_data, QP):
+        frame_height = self.config.height
+        frame_width = self.config.width
+        block_size = self.config.block_size
+        frames = []
+
+        # 1. 将帧数据分成(i x i)块
+        for i in range(0, frame_height, block_size):
+            for j in range(0, frame_width, block_size):
+                block = Block(i, j, block_size, frame_data)
+
+                # 2. 对每个块执行DCT变换
+                dct_coefficients = self.dct_transform(block.block_data)
+
+                # 3. 量化DCT系数
+                quantized_coefficients = self.quantize(dct_coefficients, QP, block_size)
+
+                # 4. TODO 保存QP用于传输给解码器
+                # 可以将QP和quantized_coefficients一起存储到一个数据结构中，然后添加到frames列表中
+                frame_info = {'QP': QP, 'coefficients': quantized_coefficients}
+                frames.append(frame_info)
+
+        return frames
+
+
+    def encode(self, input_video_path, ws='Assignment/encoder_ws'):
         # Read from video, default 420p
         frames = self.yuv_operator.read_yuv(input_video_path)
         block_size = self.config.block_size
@@ -78,18 +130,18 @@ class Encoder:
                         motion_vector, best_matching_block, residual_block = self._simple_predict_Y_(block, reference_frame, self.config.search_range)
                         residual_block = self._approximate_residual_block(residual_block)
                         reconstructed_block = self._reconstruct_block_(best_matching_block, residual_block)
-                        # print(best_matching_block.block_data)
-                        # print('--------')
-                        # print(residual_block.block_data[0][0], end=' ')
 
-                        # print(reconstructed_block.block_data)
-                        # print('***********************************')
+                        # 6. 编码I帧
+                        QP = 0  # TODO: 读取QP值，现在设置个default值，我觉得可以写到GlobalConfig里
+                        i_frame_data = self.encode_i_frame(reconstructed_block.block_data, QP)  # 传递QP值给encode_i_frame方法
+                        # TODO: 将i_frame_data保存到文件或传输给解码器
+
                         # dump blocks
                         reconstructed_frame[i:i+block_size, j:j+block_size] = reconstructed_block.block_data
                         residual_frame[i:i+block_size, j:j+block_size] = residual_block.block_data
                         motion_vectors.append(motion_vector)
                         # self.yuv_operator.visualize_YUV_in_text(best_matching_block.block_data)
-                
+
                 # post process
                 reference_frame = reconstructed_frame.copy()
                 # self.yuv_operator.convert_Y_to_png(Y_frame, f'{ws}/png_sequence/Y_frame{k}.png')
@@ -100,7 +152,6 @@ class Encoder:
                 Y_previous = Y_frame
                 residual_frames.append(residual_frame)
 
-
         # save to files
         with open(f'{ws}/residual_frames.13', 'wb') as f:
             for residual_frame in residual_frames:
@@ -108,7 +159,7 @@ class Encoder:
         with open(f'{ws}/mv.13', 'wb') as f:
             for mv in motion_vectors:
                 f.write(mv.tobytes())
-        
+
         return input_video_path
     
 
